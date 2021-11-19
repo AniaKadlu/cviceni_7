bdd451ee62ef9be97521f06eba2b1536db75553e
Anna Kadlubcova <xkadlu02@vutbr.cz>
2021-11-19 18:30:59 +0100

    Muj vlastni skript pro SSP.

---
+++ de_novo_assembly.R
@@ -0,0 +1,23 @@
+# cviceni 7, 19.11.2021
+#################################################
+#  DE NOVO ASSEMBLY  #
+#################################################
+
+# znaková metoda SSP - Shortest Supersting Problem
+# greedy algoritmus, takze jdu prvek po prvku
+
+# Hlavni funkce se bude skladat z 3 dilcich fci:
+#       - vypocet prekryvu Prekryv()
+#       - vytvoreni matice MaticePrekryvu()
+#       - spojeni sekvenci Spojeni ()
+
+################################################################################
+Prekryv = function(sek1,sek2){
+  # 1 for cyklus
+  
+  return(prekryv)
+}
+
+
+
+
