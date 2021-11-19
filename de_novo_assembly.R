548b87c107d28207e457b4d7c7a798852b7f2ec3
Anna Kadlubcova <xkadlu02@vutbr.cz>
2021-11-19 22:38:29 +0100

    Funkce Prekryv() funguje, ale snese jeste upravu.

--- de_novo_assembly.R
+++ de_novo_assembly.R
@@ -9,15 +9,50 @@
 # Hlavni funkce se bude skladat z 3 dilcich fci:
 #       - vypocet prekryvu Prekryv()
 #       - vytvoreni matice MaticePrekryvu()
-#       - spojeni sekvenci Spojeni ()
+#       - spojeni sekvenci Spojeni()
+
+################################################################################
+# sek - soubor readu, ktere skladame do contigu
+sek = c('CATGC', 'CTAAGT', 'GCTA', 'TTCA', 'ATGCATC')
+sek1 <- sek[1]
+sek2 <- sek[2]
 
 ################################################################################
 Prekryv = function(sek1,sek2){
-  # 1 for cyklus
+  # musim vyresit, kdyz jsou delky ready rozdilne o vic nez 1
+  sek1 = strsplit(sek1,"")[[1]]
+  sek2 = strsplit(sek2,"")[[1]]
   
-  return(prekryv)
+  if (length(sek2) > length(sek1)){ 
+    pom <- sek1; sek1 <- sek2; sek2 <- pom
+  }
+  L1 <- length(sek1)      # delka delsiho readu
+  L2 <- length(sek2)      # delka kratsiho readu
+  Lpre1 <- 0
+  Lpre2 <- 0
+  
+  for (i in L2:1) {   # prekryv z leva
+    if(isTRUE(sek1[1:i] == sek2[(L2+1-i):L2])){
+      prekryv1 <- sek1[1:i]           # prekryvajici usek
+      Lpre1 <- length(prekryv1)       # delka useku
+      break
+    }
+  }
+  for (j in 1:L2) {   # prekryv z prava
+    if(isTRUE(sek1[(L1-L2+j):L1] == sek2[1:(L2+1-j)])){
+      prekryv2 <- sek1[(L1-L2+j):L1]  # prekryvajici usek
+      Lpre2 <- length(prekryv2)       # delka useku
+      break
+    }
+  }
+  if (Lpre1 > Lpre2){
+    return(prekryv1)
+  }
+  
+  return(prekryv2)
 }
 
+Prekryv(sek1,sek2)
 
 
 
